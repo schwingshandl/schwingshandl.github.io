a5fd0f27f37429e95769b21a2876010bf95552ba
Richard Garber <rg.1029384756@gmail.com>
2024-12-17 20:30:30 -0500

    Further updates

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
- output: "export",
-  images: {
-    unoptimized: true,
-  },
+    experimental: {
+        reactCompiler: true,
+    },
+    output: "export",
+    images: {
+        unoptimized: true,
+    },
 };
 
 module.exports = nextConfig;
